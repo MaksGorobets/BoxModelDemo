ff9faadc7bbe75ab0ff045229f2f7f275dd86bd9
MaksGorobets <gorobets542@gmail.com>
2023-10-27 15:03:12 +0300

    Initial Commit

---
+++ BoxModelDemo/BoxModelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxModelDemoApp.swift
+//  BoxModelDemo
+//
+//  Created by Maks Winters on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BoxModelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxModelDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoxModelDemo
+//
+//  Created by Maks Winters on 27.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
